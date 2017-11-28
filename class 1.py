902eede6ee708589b52250fe24d3a19ad029123e
Shrayash Shrestha <servshres@gmail.com>
2017-11-28 10:36:44 +0545

    Add files via upload

---
+++ class 1.py
@@ -0,0 +1,27 @@
+Python 3.7.0a2 (v3.7.0a2:f7ac4fe, Oct 17 2017, 17:06:29) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+2
+4
+>>> _
+4
+>>> num1 = 7
+>>> num2 = 12
+>>> Total = num1 + num2
+>>> print(total)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    print(total)
+NameError: name 'total' is not defined
+>>> print(Total)
+19
+>>> Total
+19
+>>> _
+19
+>>> 9/5
+1.8
+>>> 9//5
+1
+>>> 9%5
+4
+>>> \\
